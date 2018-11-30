7658bb732b5d97fb7955229bffd4a399788e22c1
Miguel Dussan <migueldussan@gmail.com>
2018-11-30 23:24:16 +0000

    Add springbootdb base app

---
+++ java/springboot-db/README.md
@@ -0,0 +1,12 @@
+springboot-db
+=======
+
+This is a toy project using SpringBoot for connecting to a DB and do stuff.
+
+Requirements
+-------
+- Gradle
+- Java
+- mariadb
+
+This project requires a database `springbootdb` with user `root` and password `example`.
---
+++ java/springboot-db/src/test/java/com/migueldussan/scratch/springbootdb/SpringbootDbApplicationTests.java
@@ -0,0 +1,16 @@
+package com.migueldussan.scratch.springbootdb;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringbootDbApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
